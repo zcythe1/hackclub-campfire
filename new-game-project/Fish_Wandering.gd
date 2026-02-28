696920e5a06dd1d9eb6eed43bed27ff8ffabcce3
GeorgeHyn <waowoaw@outlook.com>
2026-02-28 17:01:28 +0000

    Fixed Fish Roaming Kind of

--- new-game-project/Fish_Wandering.gd
+++ new-game-project/Fish_Wandering.gd
@@ -12,20 +12,20 @@ func _ready() -> void:
 	starting_position = self.position
 
 func _physics_process(delta: float) -> void:
-	if travel_time <= 0: 
-		travel_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
-		travel_direction = travel_direction.normalized()
-		travel_time = randf_range(2,4)
-	
 	if on_hook:
-		starting_position = self.position
+		pass
+	else:
+		if travel_time <= 0: 
+			travel_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+			travel_direction = travel_direction.normalized()
+			travel_time = randf_range(2,4)
 	
-	if self.position.distance_to(starting_position) > wandering_circle_radius:
-		travel_direction = -travel_direction
+		if self.position.distance_to(starting_position) > wandering_circle_radius:
+			travel_direction = -travel_direction
 	
-	travel_time -= delta
+		travel_time -= delta
 	
-	self.position += travel_direction * travel_speed * delta
+		self.position += travel_direction * travel_speed * delta
 
 
 func _on_area_entered(area: Area2D) -> void:
