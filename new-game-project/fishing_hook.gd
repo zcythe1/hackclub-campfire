b427e9ce7c7bfb3ffec4fac4c3ab98c2f8f8251d
GeorgeHyn <waowoaw@outlook.com>
2026-02-28 11:27:26 +0000

    Fishing Hook Throw

--- new-game-project/fishing_hook.gd
+++ new-game-project/fishing_hook.gd
@@ -1,5 +1,24 @@
 extends Area2D
 
-func test(power):
-	print("TEST")
-	print(power)
+var launch_angle = PI/6
+var velocity = Vector2.ZERO
+var thrown = false
+
+func throw(power):
+	thrown = true
+	
+	velocity.x = 0
+	velocity.y = 0
+	velocity.x = power * cos(launch_angle)
+	velocity.y = -power * sin(launch_angle)
+	print(velocity)
+
+func _physics_process(delta: float) -> void:
+	
+	velocity.y += 9.81 * delta * 5
+	
+	if (!thrown):
+		velocity = Vector2.ZERO
+	
+	position += velocity * delta
+	velocity -= velocity * delta
--- new-game-project/fishing_rod.gd
+++ new-game-project/fishing_rod.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-var power = 1
+var power = 10
 
 @onready var fishing_hook = $"Fishing Hook"
 
 func _input(event: InputEvent):
 	while event.is_action_pressed("Fish"):
-		power += 0.5
+		power += 15
 		await get_tree().create_timer(1).timeout
 	
 	if event.is_action_released("Fish"):
@@ -14,4 +14,4 @@ func _input(event: InputEvent):
 		power = 0
 
 func launch_hook(rod_power):
-	fishing_hook.test(rod_power)
+	fishing_hook.throw(rod_power)
