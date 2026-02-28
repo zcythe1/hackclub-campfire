a83e836fc349180add74bbf2523618bbc093e50f
GeorgeHyn <waowoaw@outlook.com>
2026-02-28 16:54:31 +0000

    Fixing Fish Kinda but not

--- new-game-project/Player And Fishing/fishing_hook.gd
+++ new-game-project/Player And Fishing/fishing_hook.gd
@@ -4,6 +4,6 @@ extends Area2D
 
 
 func _on_area_entered(area: Area2D) -> void:
-	if (area.is_in_group("Materials")):
-		area.reparent(self)
-		area.position = hook.position
+	if area.is_in_group("Materials"):
+		area.call_deferred("reparent", self)
+		area.set_deferred("position", hook.position)
